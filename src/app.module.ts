77b7974f33264c780b8029f5c94eaac7214e5f71
juan <juancvillefer@gmail.com>
2026-03-17 23:38:29 -0400

    feat: Initialize NestJS application by creating the root module, configuring MongoDB, static file serving, and importing core modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { envConfig } from './config/env.config';
 import { AuthModule } from './infrastructure/auth/auth.module';
 import { SchedulingModule } from './infrastructure/scheduling/scheduling.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { HealthController } from './infrastructure/http/controllers/health.controller';
 
@@ -11,6 +13,10 @@ const config = envConfig();
 @Module({
   imports: [
     MongooseModule.forRoot(config.mongodb.uri),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
     AuthModule,
     SchedulingModule,
   ],
