feaee91393be4d53e02597399ed2b54b92117d87
juan <juancvillefer@gmail.com>
2026-03-17 23:56:06 -0400

    feat: Initialize NestJS application with global prefix, CORS, and validation pipe.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
 import { envConfig } from './config/env.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const config = envConfig();
 
   app.setGlobalPrefix('api');
   app.enableCors();
-  app.useStaticAssets(join(process.cwd(), 'uploads'), { prefix: '/uploads/' });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
